87012bd7d2c41dbfb7383e3670d8c59f6a431e4d
RevisedJDUDE2 <97071945+RevisedJDUDE2@users.noreply.github.com>
2023-01-23 20:26:25 +0800

    Add files via upload

---
+++ game.java
@@ -0,0 +1,13 @@
+import org.lwjgl.opengl.*;
+import org.lwjgl.glfw.*;
+
+public class game {
+    public static void main(String[] args) {
+        System.out.println("Version 0.1 Beta");
+        /*
+         * 
+         * No Game Yet!
+         * 
+         */
+    }
+}
